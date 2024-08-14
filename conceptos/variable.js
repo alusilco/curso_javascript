74cded00e11819f02119e1f24098cb1334211517
alusilco <coderalsc@gmail.com>
2024-08-14 20:09:49 -0300

    first commit

---
+++ conceptos/variable.js
@@ -0,0 +1,10 @@
+//variables:  var, let, const
+// En este ejemplo las variables estan inicializadas
+
+var number1 =118;
+let number2 = 119;
+const number3 =120;
+
+console.log(number1)
+console.log(number2)
+console.log(number3)
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(number1)
+console.log(number2)
+console.log(number3)
